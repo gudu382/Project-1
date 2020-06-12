70ae34b01ee2771a121ea1050486996d3bee521c
gudu382 <66767110+gudu382@users.noreply.github.com>
2020-06-12 18:27:07 +0530

    Update Editcustomer.java

--- src/test/java/Customer/Editcustomer.java
+++ src/test/java/Customer/Editcustomer.java
@@ -1,4 +1,8 @@
+
+
+public class Editcustomer{
 // Edit the customer
 // Go to task-> search customer name--> edit it
 //-----------------------------------------------
 //This is added newly code in edit customer
+}
