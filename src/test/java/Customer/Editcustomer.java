d90437a88fe5a0720dc3922915a792de786728c2
gudu382 <janmejaya3535@gmail.com>
2020-06-13 11:07:51 +0530

    write some comment

--- src/test/java/Customer/Editcustomer.java
+++
@@ -1,8 +0,0 @@
-
-
-public class Editcustomer{
-// Edit the customer
-// Go to task-> search customer name--> edit it
-//-----------------------------------------------
-//This is added newly code in edit customer
-}
